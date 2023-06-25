1a1dea53034f9ab7e7463f7898cde0dd94802ce1
Udara Suranimala <udara.suranimala1999@gmail.com>
2023-06-25 16:52:51 +0530

    added verifyToken helpers & worked on secured routes & tried fixing that token but still not fixed

--- src/redux/apiCalls/users.js
+++ src/redux/apiCalls/users.js
@@ -1,40 +1,31 @@
 import { req } from "../../axiosReqMethods";
-import { fetchStart, fetchFailed, fetchSuccess, reserError,  } from "../UseersComponentRedux";
-import { logoutUser } from "../userRedux";
+import {
+  fetchStart,
+  fetchFailed,
+  fetchSuccess,
+  reserError,
+} from "../UseersComponentRedux";
 
+export const fetchUsers = async (dispatch) => {
+  dispatch(fetchStart());
+  try {
+    const users = await req.get("/api/users/allinfo");
+    dispatch(fetchSuccess(users.data));
+    console.log("is not error in login");
+  } catch (error) { 
+      dispatch(fetchFailed(error.response.data));
 
-
-export const  fetchUsers = async (dispatch) => {
-    dispatch(fetchStart())
-    try {
-        const users = await req.get('/api/users/allinfo');
-        dispatch(fetchSuccess(users.data));
-        console.log("is not error in login")
-    } catch (error) {
-        if(error.response.data === "token is not valid") {
-            console.log("is error in login")
-            
-            dispatch(fetchFailed("your session is expired"))      
-            dispatch(logoutUser());
-            localStorage.clear();
-            
-        } else {
-            dispatch(fetchFailed(error.response.data));
-        }
-        
-        setTimeout(() => {
-            dispatch(reserError())
-        }, 5000);
-    }
-    
-
-}
+    setTimeout(() => {
+      dispatch(reserError());
+    }, 5000);
+  }
+};
 
 export const updateUser = async (userID, data) => {
-    try {
-        const res = req.put(`/api/users/${userID}`, data);
-        return res;
-    } catch(err) {
-        console.log(`error is ${err}`)
-    }
-}
+  try {
+    const res = req.put(`/api/users/${userID}`, data);
+    return res;
+  } catch (err) {
+    console.log(`error is ${err}`);
+  }
+};
